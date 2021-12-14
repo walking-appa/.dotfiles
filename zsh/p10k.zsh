e396aeb32255e50604da2acda509074b7061bb36
Dhruv <dhruvdevelopment@gmail.com>
2021-12-14 00:44:43 -0500

    Color Change

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -346,8 +346,8 @@
   #####################################[ vcs: git status ]######################################
   # Version control system colors.
   typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=10
-  typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=154
-  typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
+  typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=226
+  typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=14
   typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=196
   typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
 
